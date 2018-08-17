944f9080c604e9404c34e5299eaa710c0e6cb94a
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-17 06:37:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :user_input
   end
 
   post '/piglatinize' do
-    @latinize = PigLatinizer.new.piglatinize(params[:user_phrase])
-
-    erb :results
+    PigLatinizer.new.to_pig_latin(params[:user_phrase])
   end
+
 end
