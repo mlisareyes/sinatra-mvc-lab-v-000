6ca59a9893cad9a2191f477c2adab450f3beb248
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-17 06:30:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,7 @@
 class PigLatinizer
-  def translate(sent)
-  translation = sent.split(' ').map do |w|
-    vowels = %w(a e i o u)
-    word = w.downcase.split('')
-
-    if vowels.include? word[0]
-      "#{word.join}way"
-    else
-      "%s%say" % [word[1..-1].join, word[0]]
-    end
+  def piglatinize(word)
+    @word = word
+    if word =~ /^[aeiou]/i
+      "#{word}way"
+    elsif
   end
-  translation.join(' ').capitalize
-end
