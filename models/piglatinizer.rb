10a15242a1ca6349661b1c4773197e6a43f322bb
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-17 06:29:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,16 @@
 class PigLatinizer
 
-  def piglatinize(word)
-    @word = word
-    if word =~ /^[aeiou]/i
-      "#{word}way"
-    elsif
-      
+  def translate(sent)
+  translation = sent.split(' ').map do |w|
+    vowels = %w(a e i o u)
+    word = w.downcase.split('')
 
-
-
+    if vowels.include? word[0]
+      "#{word.join}way"
+    else
+      "%s%say" % [word[1..-1].join, word[0]]
+    end
   end
+
+  translation.join(' ').capitalize
 end
